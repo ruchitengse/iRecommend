30991ab5c979e22873802a01cfb90cb0949c6fac
Padmashri Nonabur Krishnamurthy <padmashrink@gmail.com>
2016-04-16 14:14:34 -0500

    First

---
+++ iRecommend/app/src/main/java/com/example/padmashri/irecommend/LaunchPage.java
@@ -0,0 +1,13 @@
+package com.example.padmashri.irecommend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch_page);
+    }
+}
